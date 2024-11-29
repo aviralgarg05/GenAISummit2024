9d31bafcd2f7aa20fb9dd7f7e23d5125550299b4
Saksham Verma <saksham27verma@gmail.com>
2024-11-29 17:34:02 +0530

    ticket page changes

--- src/pages/buy-ticket.tsx
+++ src/pages/buy-ticket.tsx
@@ -31,12 +31,12 @@ const BuyTickets = () => {
           <div className={styles.ticketDetails}>
             <div className={styles.passInfo}>
               <h3>Individual Pass</h3>
-              <div className={styles.price}>Rs. 15,000</div>
+              <div className={styles.price}>Rs. 6,000</div>
             </div>
-            <ul className={styles.features}>
+            {/* <ul className={styles.features}>
               <li>All access, 3 days</li>
               <li>Food access on all 3 days</li>
-            </ul>
+            </ul> */}
             <button className={styles.buyButton}>Buy</button>
           </div>
         </div>
@@ -55,12 +55,12 @@ const BuyTickets = () => {
           <div className={styles.ticketDetails}>
             <div className={styles.passInfo}>
               <h3>Group Passes</h3>
-              <div className={styles.price}>Rs. 10,500 onwards</div>
+              <div className={styles.price}>Rs. 5,400 onwards</div>
             </div>
-            <ul className={styles.features}>
+            {/* <ul className={styles.features}>
               <li>All features as Individual Pass</li>
               <li>Special discounts based on tickets bought</li>
-            </ul>
+            </ul> */}
             <button className={styles.buyButton}>Buy</button>
           </div>
         </div>
@@ -75,20 +75,20 @@ const BuyTickets = () => {
       <section className={styles.otherPasses}>
         <div className={styles.passType}>
           <div className={styles.passInfo}>
-            <h3>Regular Passes <span>16 Dec 2025 to 5 Dec 2025</span></h3>
+            <h3>Regular Passes <span>16 Dec 2024 to 5 Jan 2025</span></h3>
             <p>Group discounts reduce</p>
           </div>
-          <div className={styles.price}>Rs. 25,000</div>
+          <div className={styles.price}>Rs. 7,000</div>
         </div>
 
         <div className={styles.verticalDivider}></div>
 
         <div className={styles.passType}>
           <div className={styles.passInfo}>
-            <h3>Late Passes <span>Available from 6 Dec 2025</span></h3>
+            <h3>Late Passes <span>Available from 6 Jan 2025</span></h3>
             <p>No group discounts</p>
           </div>
-          <div className={styles.price}>Rs. 35,000</div>
+          <div className={styles.price}>Rs. 8,000</div>
         </div>
       </section>
     </div>
