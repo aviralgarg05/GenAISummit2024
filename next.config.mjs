9ed9dac4a76d51600284100d3665d9352e8469d1
Aditya Singh <103613774+AdityaSingh2005@users.noreply.github.com>
2024-12-11 21:01:11 +0530

    Sd story

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,18 @@
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // Enable static exports
-  images: {
-    unoptimized: true,  // Required for static export
-    domains: [], // Add any external domains if needed
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    formats: ['image/webp'],
-    minimumCacheTTL: 60,
-    dangerouslyAllowSVG: true,
-    contentDispositionType: 'attachment',
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-  },
-  // If you're deploying to GitHub Pages
-  basePath: '/GenAISummit2024',  // Replace with your repo name
-  assetPrefix: '/GenAISummit2024/',  // Replace with your repo name
-}
+    images: {
+      domains: [], // Add any external domains if needed
+      deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+      imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+      formats: ['image/webp'],
+      minimumCacheTTL: 60,
+      dangerouslyAllowSVG: true,
+      contentDispositionType: 'attachment',
+      contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    },
+  }
+  
 
 export default nextConfig;
