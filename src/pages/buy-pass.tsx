8ed1db7b6cacc9bf1f7dece37d2bd1ee8f2b20ef
AdityaSinghBynd <aditya@bynd.ai>
2025-01-01 03:28:04 +0530

    New year

--- src/pages/buy-pass.tsx
+++ src/pages/buy-pass.tsx
@@ -1,51 +1,54 @@
-import React, { useState,useEffect } from 'react';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/BuyTicket.module.scss';
-import Link from 'next/link';
+import React, { useState, useEffect } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/BuyTicket.module.scss";
+import Link from "next/link";
 
 const BuyTickets = () => {
-    const [groupTickets, setGroupTickets] = useState(1);
+  const [groupTickets, setGroupTickets] = useState(1);
 
-useEffect(() => {
-  const script = document.createElement("script");
-  script.src = "https://checkout.razorpay.com/v1/checkout.js";
-  script.async = true;
-  document.body.appendChild(script);
+  const [paymentSuccess, setPaymentSuccess] = useState(false);
+  const [paymentId, setPaymentId] = useState<string | null>(null);
 
-  return () => {
-    document.body.removeChild(script);
-  };
-}, []);
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://checkout.razorpay.com/v1/checkout.js";
+    script.async = true;
+    document.body.appendChild(script);
 
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
 
-  const handleIndividualPayment = () => {
+  const handlePayment = () => {
     const options = {
-  key: "rzp_live_JCuO9eUag5dosL",
-  amount: 6000 * 100, 
-  currency: "INR",
-  name: "Summit Tickets",
-  description: "Individual Pass",
-  receipt: `receipt_${new Date().getTime()}`, 
-  handler: function (response: { razorpay_payment_id: any; }) {
-    alert(`Payment successful. Payment ID: ${response.razorpay_payment_id}`);
-  },
-  prefill: {
-    name: "",
-    email: "",
-    contact: "",
-  },
-  notes: {
-    address: "Event venue",
-  },
-  theme: {
-    color: "#3399cc",
-  },
-};
-
+      key: "rzp_live_JCuO9eUag5dosL",
+      amount: 6000 * 100, // Amount in paise
+      currency: "INR",
+      name: "Summit Tickets",
+      description: "Individual Pass",
+      handler: function (response: { razorpay_payment_id: string }) {
+        setPaymentSuccess(true);
+        setPaymentId(response.razorpay_payment_id);
+      },
+      prefill: {
+        name: "",
+        email: "",
+        contact: "",
+      },
+      theme: {
+        color: "#3399cc",
+      },
+    };
 
     const razorpay = new (window as any).Razorpay(options);
     razorpay.open();
+  };
+
+  const closePopup = () => {
+    setPaymentSuccess(false);
+    setPaymentId(null);
   };
 
   const calculateGroupPrice = () => {
@@ -67,7 +70,9 @@ useEffect(() => {
       name: "Summit Tickets",
       description: `Group Pass - ${groupTickets} tickets`,
       handler: function (response: any) {
-        alert(`Payment successful. Payment ID: ${response.razorpay_payment_id}`);
+        alert(
+          `Payment successful. Payment ID: ${response.razorpay_payment_id}`
+        );
       },
       prefill: {
         name: "",
@@ -85,7 +90,7 @@ useEffect(() => {
     const razorpay = new (window as any).Razorpay(options);
     razorpay.open();
   };
-  
+
   return (
     <div className={styles.container}>
       <Head>
@@ -93,15 +98,27 @@ useEffect(() => {
         <meta name="description" content="Summit website description" />
         <link rel="icon" href="/GenAI.svg" />
       </Head>
-     <p className={styles.preRegisterContent}>For Pre Registration , fill the following details</p>
-      <Link href="/pre-register" style={{textDecoration:'none'}}> 
-      <button  className={styles.PreRegistrationBtn}>Register
-        </button></Link> 
-<p className={styles.preRegisterContent}>For final Registration , kindly purchase the tickets :</p>
-<br/>
+      <p className={styles.preRegisterContent}>
+        For Pre Registration, fill the following details :
+        <Link href="/pre-register" style={{ textDecoration: "none" }}>
+          <button className={styles.PreRegistrationBtn}>Register</button>
+        </Link>
+      </p>
+      <br />
+      <br />
+      <p className={styles.preRegisterContent}>
+        For final Registration, kindly purchase the tickets
+      </p>
+      <br />
+      <br />
       <div className={styles.earlyBirdHeader}>
-        <h2>Early Bird Passes</h2>
-        <span>Available till 31 December 2024</span>
+        <div className={styles.earlyBirdHeader2}>
+          <h2>Early Bird Passes</h2>
+          <span>Available till 5th January 2025</span>
+        </div>
+        <div>
+          <button className={styles.PreRegistrationBtn2}>New Year Offer</button>
+        </div>
       </div>
 
       <div className={styles.ticketsContainer}>
@@ -124,7 +141,9 @@ useEffect(() => {
             </div>
             <div className={styles.passInfo}>
               <div className={styles.price}>Rs. 6,000 onwards</div>
-              <button className={styles.buyButton} onClick={handleIndividualPayment}>Buy Now</button> 
+              <button className={styles.buyButton} onClick={handlePayment}>
+                Buy Now
+              </button>
             </div>
           </div>
         </div>
@@ -142,18 +161,31 @@ useEffect(() => {
           <div className={styles.ticketDetails}>
             <div className={styles.passInfo}>
               <h3>Group Pass</h3>
-              <p>10% on 3-5 Tickets bought<br />
-                15% on 6-8 Tickets bought</p>
+              <p>
+                10% on 3-5 Tickets bought
+                <br />
+                15% on 6-8 Tickets bought
+              </p>
             </div>
             <div className={styles.passInfo}>
               <div className={styles.price}></div>
               <div className={styles.ticketQuantitySelector}>
-  <button onClick={() => setGroupTickets(Math.max(1, groupTickets - 1))}>-</button>
-  <input type="number" value={groupTickets} readOnly />
-  <button onClick={() => setGroupTickets(Math.min(8, groupTickets + 1))}>+</button>
-</div>
+                <button
+                  onClick={() => setGroupTickets(Math.max(1, groupTickets - 1))}
+                >
+                  -
+                </button>
+                <input type="number" value={groupTickets} readOnly />
+                <button
+                  onClick={() => setGroupTickets(Math.min(8, groupTickets + 1))}
+                >
+                  +
+                </button>
+              </div>
 
-              <button className={styles.buyButton} onClick={handleGroupPayment}>Buy Now</button> 
+              <button className={styles.buyButton} onClick={handleGroupPayment}>
+                Buy Now
+              </button>
             </div>
           </div>
         </div>
@@ -168,7 +200,9 @@ useEffect(() => {
       <section className={styles.otherPasses}>
         <div className={styles.passType}>
           <div className={styles.passInfo}>
-            <h3>Regular Passes <span>1 Jan 2025 to 5 Jan 2025</span></h3>
+            <h3>
+              Regular Passes <span>1 Jan 2025 to 5 Jan 2025</span>
+            </h3>
             <p>Group discounts reduce</p>
           </div>
           <div className={styles.price}>Rs. 7,000</div>
@@ -178,15 +212,28 @@ useEffect(() => {
 
         <div className={styles.passType}>
           <div className={styles.passInfo}>
-            <h3>Late Passes <span>Available from 6 Jan 2025</span></h3>
+            <h3>
+              Late Passes <span>Available from 6 Jan 2025</span>
+            </h3>
             <p>No group discounts</p>
           </div>
           <div className={styles.price}>Rs. 8,000</div>
         </div>
       </section>
+      {paymentSuccess && (
+        <div className={styles.popup}>
+          <div className={styles.popupContent}>
+            <h2>Payment Successful!</h2>
+            <p>
+              Thank you for registering for the Gen AI Summit 2025. We will be
+              sending you your payment confirmation and registration ID shortly.
+            </p>
+            <button onClick={closePopup}>Close</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
 
 export default BuyTickets;
-
